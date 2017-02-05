9a55625cd5fc0a5731bcf826879b29cff5c569ac
mengwei0 <619452538@qq.com>
2017-02-05 14:14:31 +0800

    chage
    添加

--- springMVC/src/main/com/lx/controllers/publics/TopController.java
+++ springMVC/src/main/com/lx/controllers/publics/TopController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TopController {
 	@RequestMapping(value = { "index.htm" })
 	public String error() {
+//		syso
 		return "index";
 	}
 
